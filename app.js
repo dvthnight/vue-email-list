d21b5117b1789081e2884b392740df1fde8a0b2d
dvthnight <davide.lanotte@protonmail.com>
2022-03-04 16:52:58 +0100

    creata funzione che genera array

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const app = new Vue ({
+    el: "#app",
+
+    data: {
+        indirizzi: [],
+        indirizzo: " ",
+        banana: "ciao sono banana"
+    },
+
+    created() {
+        
+           for(let i=0; i<10; i++){
+
+           
+
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((res)=>{
+                    this.indirizzo = res.data.response;
+
+                    console.log(i);
+
+                    this.indirizzi[i] = this.indirizzo;
+                    console.log(this.indirizzi);
+                });
+
+
+                console.log(this.indirizzi)
+           }
+
+        
+    },
+})
